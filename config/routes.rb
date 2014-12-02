351a92cd679a338d678b954fcb00d25529dbce00
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2014-12-02 09:56:12 -0500

    please just save this fucking thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  root to: 'user#index'
+
+  resources :pins
+
+  resources :boards
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
